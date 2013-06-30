7517e8c2637780e4d2ee1971c3c1bcaf5fc6a279
rshiva <nadarshiva.r@gmail.com>
2013-06-30 23:12:16 +0530

    added mailer setting and class

--- lib/gratitude_rubyists.rb
+++ lib/gratitude_rubyists.rb
@@ -10,7 +10,8 @@ File.open(gem_file_path).read.each_line do |i|
 	  gem_name=i.to_s.split(",").first
 	  gem_name=eval gem_name
 	  author_email_id=Gem::Specification.find_by_name(gem_name).email
-    puts "===>",author_email_id.class
+	  author_name=Gem::Specification.find_by_name(gem_name).names
+    puts "===>",author_email_id
   elsif i.start_with?("#")
   	next
   end
@@ -18,5 +19,19 @@ end
 
 # Rails.application.config.action_mailer[:perform_deliveries]
 class GratitudeMail < ActionMailer::Base
+
+	def send_mail(author_email_id,author_name)
+		@to = author_email_id if author_email_id
+		@from =  ActionMailer::Base.smtp_settings[:user_name]
+		@author_name =  author_name if author_name
+		@subject    = " Hey #{@author_name} ,Thanks for this awesome gem "
+		puts "===>",@to,@from,@subject
+#		mail(:to => @to ,:from => ,:subject => @subject)
+		
+	end
+
+	def message
+
+	end
 end
 end
